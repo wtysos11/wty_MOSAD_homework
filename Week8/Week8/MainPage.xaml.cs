f10245768238a7abf10f37a7dcbb46fecb855554
wty <wtysos11@163.com>
2018-05-01 23:55:54 +0800

    finish Week8's homework

--- Week8/Week8/MainPage.xaml.cs
+++ Week8/Week8/MainPage.xaml.cs
@@ -42,9 +42,35 @@ namespace Week8
         public MainPage()
         {
             this.InitializeComponent();
-            toggle = true;
+            toggle = true;//poster在前台为true，poster在后台为false
         }
         //a converter for mediaplayer to change double to TimeSpan.
+
+        private void toggle_solve()
+        {
+            //toggle为false时是电影模式，切换时mediaPlayer要在前台.
+            if(toggle == true)
+            {
+                mediaPlayer.Width = media_poster.Width;
+                mediaPlayer.Height = media_poster.Height;
+                mediaPlayer.Margin = media_poster.Margin;
+                media_poster.Width = 0;
+                media_poster.Height = 0;
+                media_poster.Margin = new Thickness(0);
+                toggle = false;
+            }
+            else//mediaPlayer在后台
+            {
+                myStoryboard.Stop();
+                media_poster.Width = mediaPlayer.Width;
+                media_poster.Height = mediaPlayer.Height;
+                media_poster.Margin = mediaPlayer.Margin;
+                mediaPlayer.Width = 0;
+                mediaPlayer.Height = 0;
+                mediaPlayer.Margin = new Thickness(0);
+                toggle = true;
+            }
+        }
 
         private void media_MediaOpened(object sender, RoutedEventArgs e)
         {
@@ -53,16 +79,19 @@ namespace Week8
 
         private void media_Play(object sender, RoutedEventArgs e)
         {
+            myStoryboard.Begin();
             mediaPlayer.Play();
         }
 
         private void media_Pause(object sender, RoutedEventArgs e)
         {
+            myStoryboard.Pause();
             mediaPlayer.Pause();
         }
 
         private void media_Stop(object sender, RoutedEventArgs e)
         {
+            myStoryboard.Stop();
             mediaPlayer.Stop();
         }
 
@@ -81,12 +110,22 @@ namespace Week8
             {
                 var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
                 mediaPlayer.SetSource(stream, file.ContentType);
-
+                string type = file.ContentType;
+                int indexOf = type.IndexOf("/");
+                string originType = type.Substring(0, indexOf);
+                if((originType.Equals("audio")&&toggle==false)||(originType.Equals("video") && toggle == true))
+                {
+                    toggle_solve();
+                }
                 mediaPlayer.Play();
             }
         }
         private void media_Video(object sender, RoutedEventArgs e)
         {
+            if (toggle == true)
+            {
+                toggle_solve();
+            }
             mediaPlayer.Source = new Uri("ms-appx:///Assets/Videos/1.mp4");
             mediaPlayer.Play();
         }
@@ -94,6 +133,15 @@ namespace Week8
         private void media_FullScreen(object sender, RoutedEventArgs e)
         {
             mediaPlayer.IsFullWindow = !mediaPlayer.IsFullWindow;
+        }
+        private void ChangeMediaVolume(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Volume = (double)volumeSlider.Value;
+        }
+
+        private void media_Volume(object sender, RoutedEventArgs e)
+        {
+            volumeControl.Opacity = (volumeControl.Opacity == 0) ? 1 : 0;
         }
     }
 }
